d5550130d90f044d61cba63921d7f5b73404be34
Ganesh Jadhav <98861204+Jadhavggg@users.noreply.github.com>
2023-05-25 17:42:43 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,8 +10,8 @@ sleep 1
 # Clone kernel
 git clone https://github.com/Jadhavggg/android_kernel_oneplus_6877 -b kernel/oneplus/mt6877
 
-# Setup kernel
-(cd kernel/oneplus/ivan; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
+
+
 
 # Clone vendor
 git clone https://github.com/Jadhavggg/android_vendor_oneplus_ivan -b pixel vendor/oneplus/ivan
